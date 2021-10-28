2d2aeb4b259d9e02e64c49240b91bd0dacf5e645
Brian Bycynski <high5bri@hotmail.com>
2021-10-28 02:24:47 -0400

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+//
+//
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import React from 'react';
+import './App.css';
+import ExpenseItem from './components/Expenses/ExpenseItem';
 
 const App = () => {
   const expenses = [
@@ -25,25 +27,31 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
-
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <h1 className='App__h1'>Expense Tracker</h1>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
+//
+//
+
 import React from 'react';
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = 'Car Insurance Bozo';
+  // const expenseAmount = 294.99;
+
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
+    <div className='expense-item'>
+      <div className=''>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </Card>
+    </div>
   );
-}
+};
 
 export default ExpenseItem;
