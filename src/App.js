b93a4e66548b88ff5066400682025aaaa75e63be
Brian Bycynski <high5bri@hotmail.com>
2021-11-01 02:48:34 -0400

    state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 //
 
 import React from 'react';
-import ExpenseItem from './components/Expenses/ExpenseItem';
+
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
   const expenses = [
@@ -29,26 +30,8 @@ const App = () => {
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h1 style={{ textAlign: 'center', color: '#fff' }}>Let's Go</h1>
+      <Expenses items={expenses} />
     </div>
   );
 };
